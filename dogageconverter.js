0877d28693ab15b360deb76715388f19fddd831b
allwintitus <109658761+allwintitus@users.noreply.github.com>
2022-10-19 09:54:05 +0530

    Create dogageconverter.js
    this is the java script code to convert dog age to human age

---
+++ dogageconverter.js
@@ -0,0 +1,4 @@
+/* the dog age to human age converter*/
+var dogage= prompt("enter the dog dogage");//getting the dog age
+var human_age=(dogage-2)*4+21; //calculating using the formula or human age coverter
+alert("your dog age is"+ human_age + "years old in human years"); //final result in the alert box
